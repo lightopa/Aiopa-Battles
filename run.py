6f1b0fb4f433859d7f80c7d1538f5de9a40509f6
James <james.waters@talktalk.net>
2016-10-30 15:30:36 +0000

    Put whole running script within an if name == main clause

--- run.py
+++ run.py
@@ -6,17 +6,18 @@ import states
 
 #states.boot()
 
-if not os.path.exists("logs"):
-    os.makedirs("logs")
-
-cProfile.run("states.boot()", "logs/Out.txt")
-
-with open("logs/Calltime Dump.txt", "w") as fc:
-    p = pstats.Stats("logs/Out.txt", stream=fc)
-    p.strip_dirs()
-    d = p.__dict__["stats"]
-    funcstats = {}
+if __name__ == "__main__":
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
     
-    for k, v in d.items():
-        funcstats[k] = v[2]
-    p.sort_stats("time").print_stats()
+    cProfile.run("states.boot()", "logs/Out.txt")
+    
+    with open("logs/Calltime Dump.txt", "w") as fc:
+        p = pstats.Stats("logs/Out.txt", stream=fc)
+        p.strip_dirs()
+        d = p.__dict__["stats"]
+        funcstats = {}
+        
+        for k, v in d.items():
+            funcstats[k] = v[2]
+        p.sort_stats("time").print_stats()
